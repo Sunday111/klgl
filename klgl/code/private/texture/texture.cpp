06b12481df3470911e1f4b1acfbead976e7207a9
Kostiantyn Lazukin <konstantin.lazukin@gmail.com>
2024-08-17 01:07:18 +0100

    Texture internal formats

--- klgl/code/private/texture/texture.cpp
+++ klgl/code/private/texture/texture.cpp
@@ -11,14 +11,17 @@
 namespace klgl
 {
 
-constexpr auto MakeFormatToNumChannelsInfo()
+using GlIntHasher = decltype([](const GLint v) { return std::bit_cast<size_t>(static_cast<int64_t>(v)); });
+using GlEnumHasher = decltype([](const GLenum v) -> size_t { return v; });
+
+inline constexpr auto kTextureBufferFormatToNumChannels = []
 {
-    ass::FixedUnorderedMap<14, GLint, uint8_t, decltype([](const GLint v) { return static_cast<size_t>(v); })> m;
+    ass::FixedUnorderedMap<14, GLint, uint8_t, GlIntHasher> map;
 
     auto add = [&](GLint key, auto value)
     {
-        assert(!m.Contains(key));
-        [[maybe_unused]] const bool success = m.TryAdd(key, value);
+        assert(!map.Contains(key));
+        [[maybe_unused]] const bool success = map.TryAdd(key, value);
         assert(success);
     };
 
@@ -41,23 +44,177 @@ constexpr auto MakeFormatToNumChannelsInfo()
 
     add(GL_DEPTH_STENCIL, 2);
 
-    return m;
-}
+    return map;
+}();
 
-inline constexpr auto kFormatToNumChannels = MakeFormatToNumChannelsInfo();
-
-std::optional<size_t> PixelBufferFormat::TryGetNumChannels(std::string* out_error) const
+inline constexpr auto kTextureChannelTypeToBitsCount = []
 {
-    if (!kFormatToNumChannels.Contains(channels_format))
+    ass::FixedUnorderedMap<14, GLenum, uint8_t, GlEnumHasher> map;
+
+    auto add = [&](const GLenum key, const uint8_t value)
+    {
+        assert(!map.Contains(key));
+        map.Add(key, value);
+    };
+
+    add(GL_UNSIGNED_BYTE, 8);
+    add(GL_FLOAT, 32);
+
+    return map;
+}();
+
+inline constexpr auto kTextureInteralFormatToFormatInfo = []
+{
+    ass::FixedUnorderedMap<67, GLint, TextureFormatInfo, GlIntHasher> map;
+
+    auto add = [&]<typename... Args>(const GLint key, TextureFormatInfo value)
+    {
+        assert(!map.Contains(key));
+        map.Add(key, value);
+    };
+
+    // Base internal formats
+    // See table 1 at https://registry.khronos.org/OpenGL-Refpages/gl4/html/glTexImage2D.xhtml
+    add(GL_DEPTH_COMPONENT, {.n = 1, .base = 1});
+    add(GL_DEPTH_STENCIL, {.n = 2, .base = 1});
+    add(GL_RED, {.n = 1, .base = 1});
+    add(GL_RG, {.n = 2, .base = 1});
+    add(GL_RGB, {.n = 3, .base = 1});
+    add(GL_RGBA, {.n = 4, .base = 1});
+
+    // Sized internal formats
+    // See table 2 at https://registry.khronos.org/OpenGL-Refpages/gl4/html/glTexImage2D.xhtml
+    add(GL_R8, {.n = 1, .r = 8, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_R8_SNORM, {.n = 1, .r = 8, .norm = 1, .sign = 1, .fp = 0});
+    add(GL_R16, {.n = 1, .r = 16, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_R16_SNORM, {.n = 1, .r = 16, .norm = 1, .sign = 1, .fp = 0});
+    add(GL_RG8, {.n = 2, .r = 8, .g = 8, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RG8_SNORM, {.n = 2, .r = 8, .g = 8, .norm = 1, .sign = 1, .fp = 0});
+    add(GL_RG16, {.n = 2, .r = 16, .b = 16, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RG16_SNORM, {.n = 2, .r = 16, .b = 16, .norm = 1, .sign = 1, .fp = 0});
+    add(GL_R3_G3_B2, {.n = 3, .r = 3, .g = 3, .b = 2, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGB4, {.n = 3, .r = 4, .g = 4, .b = 4, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGB5, {.n = 3, .r = 5, .g = 5, .b = 5, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGB8, {.n = 3, .r = 8, .g = 8, .b = 8, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGB8_SNORM, {.n = 3, .r = 8, .g = 8, .b = 8, .norm = 1, .sign = 1, .fp = 0});
+    add(GL_RGB10, {.n = 3, .r = 10, .g = 10, .b = 10, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGB12, {.n = 3, .r = 12, .g = 12, .b = 12, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGB16_SNORM, {.n = 3, .r = 16, .g = 16, .b = 16, .norm = 1, .sign = 1, .fp = 0});
+    add(GL_RGBA2, {.n = 4, .r = 2, .g = 2, .b = 2, .a = 2, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGBA4, {.n = 4, .r = 4, .g = 4, .b = 4, .a = 4, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGB5_A1, {.n = 4, .r = 5, .g = 5, .b = 5, .a = 1, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGBA8, {.n = 4, .r = 8, .g = 8, .b = 8, .a = 8, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGBA8_SNORM, {.n = 4, .r = 8, .g = 8, .b = 8, .a = 8, .norm = 1, .sign = 1, .fp = 0});
+    add(GL_RGB10_A2, {.n = 4, .r = 10, .g = 10, .b = 10, .a = 2, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGB10_A2UI, {.n = 4, .r = 10, .g = 10, .b = 10, .a = 2, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RGBA12, {.n = 4, .r = 12, .g = 12, .b = 12, .a = 12, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_RGBA16, {.n = 4, .r = 16, .g = 16, .b = 16, .a = 16, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_SRGB8, {.n = 3, .r = 8, .g = 8, .b = 8, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_SRGB8_ALPHA8, {.n = 4, .r = 8, .g = 8, .b = 8, .a = 8, .norm = 1, .sign = 0, .fp = 0});
+    add(GL_R16F, {.n = 1, .r = 16, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_RG16F, {.n = 2, .r = 16, .g = 16, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_RGB16F, {.n = 3, .r = 16, .g = 16, .b = 16, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_RGBA16F, {.n = 4, .r = 16, .g = 16, .b = 16, .a = 16, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_R32F, {.n = 1, .r = 32, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_RG32F, {.n = 2, .r = 32, .g = 32, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_RGB32F, {.n = 3, .r = 32, .g = 32, .b = 32, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_RGBA32F, {.n = 4, .r = 32, .g = 32, .b = 32, .a = 32, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_R11F_G11F_B10F, {.n = 3, .r = 11, .g = 11, .b = 10, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_RGB9_E5, {.n = 3, .r = 9, .g = 9, .b = 9, .a = 5, .norm = 0, .sign = 1, .fp = 1});
+    add(GL_R8I, {.n = 1, .r = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_R8UI, {.n = 1, .r = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_R16I, {.n = 1, .r = 16, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_R16UI, {.n = 1, .r = 16, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_R32I, {.n = 1, .r = 32, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_R32UI, {.n = 1, .r = 32, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RG8I, {.n = 2, .r = 8, .g = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RG8UI, {.n = 2, .r = 8, .g = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RG16I, {.n = 2, .r = 16, .g = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RG16UI, {.n = 2, .r = 16, .g = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RG32I, {.n = 2, .r = 32, .g = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RG32UI, {.n = 2, .r = 32, .g = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RGB8I, {.n = 3, .r = 8, .g = 8, .b = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RGB8UI, {.n = 3, .r = 8, .g = 8, .b = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RGB16I, {.n = 3, .r = 16, .g = 8, .b = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RGB16UI, {.n = 3, .r = 16, .g = 8, .b = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RGB32I, {.n = 3, .r = 32, .g = 8, .b = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RGB32UI, {.n = 3, .r = 32, .g = 8, .b = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RGBA8I, {.n = 4, .r = 8, .g = 8, .b = 8, .a = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RGBA8UI, {.n = 4, .r = 8, .g = 8, .b = 8, .a = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RGBA16I, {.n = 4, .r = 16, .g = 8, .b = 8, .a = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RGBA16UI, {.n = 4, .r = 16, .g = 8, .b = 8, .a = 8, .norm = 0, .sign = 0, .fp = 0});
+    add(GL_RGBA32I, {.n = 4, .r = 32, .g = 8, .b = 8, .a = 8, .norm = 0, .sign = 1, .fp = 0});
+    add(GL_RGBA32UI, {.n = 4, .r = 32, .g = 8, .b = 8, .a = 8, .norm = 0, .sign = 0, .fp = 0});
+
+    // Maybe worth adding compressed formats, idk
+
+    return map;
+}();
+
+std::optional<TextureFormatInfo> TextureFormatHelper::TryGetTextureInternalFormatInfo(
+    const GLint internal_format,
+    std::string* out_error)
+{
+    if (!kTextureInteralFormatToFormatInfo.Contains(internal_format))
     {
         if (out_error)
         {
-            fmt::format_to(std::back_inserter(*out_error), "Unsupported pixel data format {}", channels_format);
+            fmt::format_to(std::back_inserter(*out_error), "Unsupported internal format {}", internal_format);
         }
         return std::nullopt;
     }
 
-    return kFormatToNumChannels.Get(channels_format);
+    return kTextureInteralFormatToFormatInfo.Get(internal_format);
+}
+
+TextureFormatInfo TextureFormatHelper::GetTextureInternalFormatInfo(const GLint internal_format)
+{
+    std::string error;
+    auto opt = TryGetTextureInternalFormatInfo(internal_format, &error);
+    klgl::ErrorHandling::Ensure(opt.has_value(), "{}", error);
+    return *opt;
+}
+
+std::optional<PixelBufferFormatInfo> TextureFormatHelper::TryGetPixelBufferFormatInfo(
+    const GLint channels_format,
+    const GLenum channel_type,
+    std::string* out_error)
+{
+    size_t num_channels = 0;
+    size_t bits_per_channel = 0;
+    if (!TryGetPixelBufferChannelsCount(channels_format, out_error))
+    {
+        return std::nullopt;
+    }
+
+    if (!TryGetPixelBufferChannelSize(channel_type, out_error))
+    {
+        return std::nullopt;
+    }
+
+    return PixelBufferFormatInfo{
+        .num_channels = num_channels,
+        .bits_per_channel = bits_per_channel,
+    };
+}
+
+std::optional<size_t> TextureFormatHelper::TryGetPixelBufferChannelsCount(const GLint format, std::string* out_error)
+{
+    if (!kTextureBufferFormatToNumChannels.Contains(format))
+    {
+        if (out_error)
+        {
+            fmt::format_to(std::back_inserter(*out_error), "Unsupported pixel image format {}", format);
+        }
+        return std::nullopt;
+    }
+
+    return kTextureBufferFormatToNumChannels.Get(format);
+}
+
+std::optional<size_t> PixelBufferFormat::TryGetNumChannels(std::string* out_error) const
+{
+    return TextureFormatHelper::TryGetPixelBufferChannelsCount(channels_format, out_error);
 }
 
 size_t PixelBufferFormat::GetNumChannels() const
@@ -66,6 +223,22 @@ size_t PixelBufferFormat::GetNumChannels() const
     auto opt = TryGetNumChannels(&error);
     klgl::ErrorHandling::Ensure(opt.has_value(), "{}", error);
     return *opt;
+}
+
+std::optional<size_t> TextureFormatHelper::TryGetPixelBufferChannelSize(
+    const GLenum channel_type,
+    std::string* out_error)
+{
+    if (!kTextureChannelTypeToBitsCount.Contains(channel_type))
+    {
+        if (out_error)
+        {
+            fmt::format_to(std::back_inserter(*out_error), "Unsupported channel type {}", channel_type);
+        }
+        return std::nullopt;
+    }
+
+    return kTextureChannelTypeToBitsCount.Get(channel_type);
 }
 
 std::optional<size_t> PixelBufferFormat::TryGetChannelSize(std::string* out_error) const
@@ -114,9 +287,26 @@ void PixelBufferFormat::ValidateBufferSize(const Vec2<size_t>& resolution, const
         num_bytes);
 }
 
-std::unique_ptr<Texture> Texture::CreateEmpty(const Vec2<size_t>& resolution, const PixelBufferFormat& format)
+void PixelBufferFormat::EnsureCompatibleWithInternalTextureFormat(const GLint internal_texture_format) const
+{
+    const size_t num_channels = GetNumChannels();
+    // const size_t channel_size = GetChannelSize();
+    const TextureFormatInfo texture_format_info =
+        TextureFormatHelper::GetTextureInternalFormatInfo(internal_texture_format);
+
+    klgl::ErrorHandling::Ensure(
+        num_channels == texture_format_info.n,
+        "Attempt to set pixel data of texture with {} channels providing data with {} channels only",
+        texture_format_info.n,
+        num_channels);
+}
+
+std::unique_ptr<Texture> Texture::CreateEmpty(const Vec2<size_t>& resolution, const GLint format)
 {
     ErrorHandling::Ensure(resolution != Vec2<size_t>{}, "Empty texture size!");
+
+    const TextureFormatInfo texture_format_info = TextureFormatHelper::GetTextureInternalFormatInfo(format);
+    klgl::ErrorHandling::Ensure(!texture_format_info.base, "Do not use base internal formats.");
 
     auto tex = std::make_unique<Texture>();
     tex->texture_ = OpenGl::GenTexture();
@@ -128,15 +318,7 @@ std::unique_ptr<Texture> Texture::CreateEmpty(const Vec2<size_t>& resolution, co
     assert(tex->type_ == GL_TEXTURE_2D);
     // auto err = glGetError();
     // assert(err == GL_NO_ERROR);
-    OpenGl::TexImage2d(
-        tex->type_,
-        0,
-        format.channels_format,
-        resolution.x(),
-        resolution.y(),
-        format.channels_format,
-        format.channel_type,
-        nullptr);
+    OpenGl::TexImage2d(tex->type_, 0, format, resolution.x(), resolution.y(), GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
     // err = glGetError();
     // assert(err == GL_NO_ERROR);
 
@@ -156,8 +338,8 @@ void Texture::Bind() const
 
 void Texture::SetPixels(const PixelBufferFormat& format, std::span<const uint8_t> data)
 {
-    ErrorHandling::Ensure(format == format_, "Format {} does not match internal format {}", format, format_);
     format.ValidateBufferSize(resolution_, data.size_bytes());
+    format.EnsureCompatibleWithInternalTextureFormat(format_);
 
     assert(type_ == GL_TEXTURE_2D);
     Bind();
--- klgl/code/public/klgl/texture/texture.hpp
+++ klgl/code/public/klgl/texture/texture.hpp
@@ -12,17 +12,49 @@
 namespace klgl
 {
 
+struct TextureFormatInfo
+{
+    uint8_t n : 3 = 0;  // num_components
+    uint8_t r : 6 = 0;  // num bits in red component
+    uint8_t g : 6 = 0;  // num bits in green component
+    uint8_t b : 6 = 0;  // num bits in blue component
+    uint8_t a : 6 = 0;  // num bits in alpha component
+    bool base : 1 = 0;  // Is base internal format. Like GL_RGBA, GL_RG, etc.
+    bool norm : 1 = 0;  // is normalized
+    bool sign : 1 = 0;  // is signed
+    bool fp : 1 = 0;    // is floating point
+};
+
+struct PixelBufferFormatInfo
+{
+    size_t num_channels = 0;
+    size_t bits_per_channel = 0;
+};
+
+struct TextureFormatHelper
+{
+    static std::optional<PixelBufferFormatInfo>
+    TryGetPixelBufferFormatInfo(const GLint channels_format, const GLenum channel_type, std::string* out_error);
+    static std::optional<size_t> TryGetPixelBufferChannelsCount(const GLint format, std::string* out_error);
+    static std::optional<size_t> TryGetPixelBufferChannelSize(const GLenum channel_type, std::string* out_error);
+    static std::optional<TextureFormatInfo> TryGetTextureInternalFormatInfo(
+        const GLint internal_format,
+        std::string* out_error);
+    static TextureFormatInfo GetTextureInternalFormatInfo(const GLint internal_format);
+};
+
 struct PixelBufferFormat
 {
-    constexpr bool operator<=>(const PixelBufferFormat&) const = default;
-
+    // The number of channels in image format
     std::optional<size_t> TryGetNumChannels(std::string* out_error) const;
     size_t GetNumChannels() const;
 
+    // The number of bits in one channel for this format
     std::optional<size_t> TryGetChannelSize(std::string* out_error) const;
     size_t GetChannelSize() const;
 
     void ValidateBufferSize(const Vec2<size_t>& resolution, const size_t num_bytes) const;
+    void EnsureCompatibleWithInternalTextureFormat(const GLint internal_texture_format) const;
 
     template <typename FmtContext>
     constexpr auto FormatTo(FmtContext& ctx) const
@@ -41,7 +73,9 @@ struct PixelBufferFormat
 class Texture
 {
 public:
-    static std::unique_ptr<Texture> CreateEmpty(const Vec2<size_t>& resolution, const PixelBufferFormat& format);
+    TextureFormatInfo GetFormatInfo() const { return TextureFormatHelper::GetTextureInternalFormatInfo(format_); }
+
+    static std::unique_ptr<Texture> CreateEmpty(const Vec2<size_t>& resolution, const GLint format = GL_RGBA8);
 
     ~Texture();
 
@@ -62,7 +96,7 @@ private:
     std::optional<GLuint> texture_;
     Vec2<size_t> resolution_;
     GLenum type_ = GL_TEXTURE_2D;
-    PixelBufferFormat format_{};
+    GLint format_ = GL_RGBA;
 };
 
 }  // namespace klgl
